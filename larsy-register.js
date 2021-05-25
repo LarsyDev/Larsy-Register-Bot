4f7aeab15055b5a3f14753d6c1da1a84441adc14
LarsyDev <84153722+LarsyDev@users.noreply.github.com>
2021-05-25 22:47:13 +0200

    Update larsy-register.js

--- larsy-register.js
+++ larsy-register.js
@@ -159,7 +159,7 @@ client.on("message", message => {
                       **▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬**
                    **Durumum: \` ${Settings.botStatus} \`**
                       **▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬**
-                   **AltYapı Sahibi:** [Larsy](https://github.com/larsydev) ( <@846777074613354526> )
+                   **AltYapı Sahibi:** [Larsy](https://github.com/larsydev) ( <@846777074613354526> ) 
                       **▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬**`)
   .setColor("#323131")
   .setTimestamp()
